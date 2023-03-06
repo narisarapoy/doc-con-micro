0b6c87e3a0899590be5e1755730cfb67671373cd
Jirachaya Khamsopa <jirachaya.kh@gmail.com>
2023-03-06 15:49:03 +0700

    remove file

--- doc_con_micro/doc_con_micro/settings.py
+++ doc_con_micro/doc_con_micro/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'doc_con_micro',
-    'django_summernote',
 ]
 
 MIDDLEWARE = [
@@ -125,12 +126,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#Handle session is not Json Serializable
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
-
-# Auto logout delay in minutes
-AUTO_LOGOUT_DELAY = 1 #equivalent to 5 minutes
-
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-SESSION_COOKIE_AGE = 5 * 60
